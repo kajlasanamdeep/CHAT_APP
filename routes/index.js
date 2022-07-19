89195544dbed93eca1ee0ac55f8c5bd7035d2ab0
kajlasanamdeep <kajlasanamdeep@gmail.com>
2022-07-20 00:18:23 +0530

    ui changes

--- routes/index.js
+++ routes/index.js
@@ -4,75 +4,101 @@ const sequelize = require('sequelize');
 const Op = sequelize.Op;
 
 router
-.get('/', (req, res) => {
-    return res.render('login', { message: "LOGIN FORM" })
-})
-.post('/', async (req, res) => {
-    try {
-        console.log(req.body);
-        let user = await Users.findOne({
-            where: req.body,
-            include: {
-                model: Contacts,
-                include: 'contact'
-            }
-        });
-        if (!user) return res.render('login', { message: 'USER NOT FOUND' });
-        user = user.toJSON()
-        let messages = await Messages.findAll({
-            where: {
-                [Op.or]: [{ to: user.id }, { from: user.id }]
-            },
-            separate: true,
-            order: [['at', 'ASC']],
-            include: ['receiver', 'sender']
-        });
-        console.log(user);
-        return res.render('chat', { id: user.id, username: user.username, contacts: user.contacts, messages: messages });
-    } catch (error) {
-        throw error;
-    }
-})
-
-
-.get('/signup', (req, res) => {
-    return res.render('signup', { message: "SIGNUP FORM" });
-})
-.post('/signup', async (req, res) => {
-    try {
-        let exitingUser = await Users.findOne({
-            where: {
-                username: req.body.username
-            }
-        });
-        if (exitingUser) return res.render('signup', { message: 'CHOOSE OTHER USERNAME' });
-        let newUser = await Users.create(req.body);
-        newUser = newUser.toJSON()
-        return res.render('chat', newUser);
-    } catch (error) {
-        throw error;
-    }
-})
-
-
-.post('/addContact', async (req, res) => {
-    try {
-        let newContact = await Users.findOne({
-            where: {
-                username: req.body.contactName
-            }
-        });
-        if (newContact) {
-            newContact = newContact.toJSON()
-            Contacts.create({
-                userID: req.body.userID,
-                contactID: newContact.id
+    .get('/', (req, res) => {
+        return res.render('login', { message: "LOGIN FORM" })
+    })
+    .post('/login', async (req, res) => {
+        try {
+            let user = await Users.findOne({
+                where: req.body,
+                include: {
+                    model: Contacts,
+                    include: 'contact'
+                }
             });
+            user = user.toJSON();
+            if (!user) return res.render('login', { message: 'USER NOT FOUND' });
+            let messages = await Messages.findAll({
+                where: {
+                    [Op.or]: [{ to: user.id }, { from: user.id }]
+                },
+                separate: true,
+                order: [['at', 'ASC']],
+                include: ['receiver', 'sender']
+            });
+            console.log(user);
+            return res.render('chat', { id: user.id, username: user.username, contacts: user.contacts, messages: messages });
+        } catch (error) {
+            throw error;
         }
-        return res.redirect('/');
-    } catch (error) {
-        throw error
-    }
-});
+    })
+
+
+    .get('/signup', (req, res) => {
+        return res.render('signup', { message: "SIGNUP FORM" });
+    })
+    .post('/signup', async (req, res) => {
+        try {
+            let exitingUser = await Users.findOne({
+                where: {
+                    username: req.body.username
+                }
+            });
+            if (exitingUser) return res.render('signup', { message: 'CHOOSE OTHER USERNAME' });
+            let newUser = await Users.create(req.body);
+            newUser = newUser.toJSON()
+            return res.render('chat', newUser);
+        } catch (error) {
+            throw error;
+        }
+    })
+
+
+    .post('/addContact', async (req, res) => {
+        try {
+            let newContact = await Users.findOne({
+                where: {
+                    username: req.body.contactName
+                }
+            });
+            if (newContact) {
+                let existingContact = await Contacts.findOne({
+                    where: {
+                        userID: req.body.userID,
+                        contactID: newContact.id
+                    }
+                });
+                if (!existingContact) {
+                    newContact = newContact.toJSON()
+                    await Contacts.create({
+                        userID: req.body.userID,
+                        contactID: newContact.id
+                    });
+                }
+            }
+            let user = await Users.findOne({
+                where: {
+                    id: req.body.userID
+                },
+                include: {
+                    model: Contacts,
+                    include: 'contact'
+                }
+            });
+            user = user.toJSON()
+            let messages = await Messages.findAll({
+                where: {
+                    [Op.or]: [{ to: user.id }, { from: user.id }]
+                },
+                separate: true,
+                order: [['at', 'ASC']],
+                include: ['receiver', 'sender']
+            });
+
+            return res.render('chat', { id: user.id, username: user.username, contacts: user.contacts, messages: messages });
+        } catch (error) {
+            throw error
+        }
+    });
 
 module.exports = router;
--- socket.io/socket.io.js
+++ socket.io/socket.io.js
@@ -1,4 +1,4 @@
-const {Messages} = require('../models/index');
+const {Messages, Contacts} = require('../models/index');
 const users = {};
 module.exports = (socket) => {
     socket.on('active_user', (user) => {
@@ -7,14 +7,20 @@ module.exports = (socket) => {
         socket.userID = user.userID;
         users[user.userID] = socket.id;
     });
-    socket.on('send_message', (data) => {
+    socket.on('send_message',async(data) => {
         let socketID = users[data.contactID];
         socket.to(socketID).emit('message', {
             contactID: socket.userID,
             contactName: socket.username,
             message: data.message
         });
-        Messages.create({
+        await Contacts.findOrCreate({
+            where:{
+                userID: data.contactID,
+                contactID: socket.userID
+            }
+        })
+        await Messages.create({
             message: data.message,
             to: data.contactID,
             from: socket.userID
